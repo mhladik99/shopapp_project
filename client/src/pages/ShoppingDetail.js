3c1a99226b120016b5fd1328e32fe054a880f91b
Michal Hladík <mltgroup@email.cz>
2023-11-02 19:37:29 +0100

    added-main-buttons

--- client/src/pages/ShoppingDetail.js
+++ client/src/pages/ShoppingDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import EditableTitle from '../components/editableTitle/editableTitle';
 import Title from '../components/Title/Title';
 import ShoppingList from '../components/shoppingList/shoppingList';
+import Button from '../components/Button/Button';
 
 const ShoppingDetail = () => {
   return (
@@ -9,6 +10,10 @@ const ShoppingDetail = () => {
       <EditableTitle />
       <Title title="Položky" />
       <ShoppingList/>
+        <div className='button-container'>
+          <Button className="main-button">Archivovat</Button>
+          <Button className="main-button">Smazat</Button>
+        </div>
     </div>
   );
 };
