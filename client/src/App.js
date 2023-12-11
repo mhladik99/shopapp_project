5505325a556a935d0e5ac8f5d70ad314833bb7cd
Michal Hladík <mltgroup@email.cz>
2023-12-11 21:40:26 +0100

    toggleSwitch

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Main from './pages/Main';
 import ShoppingDetail from './pages/ShoppingDetail';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { NotificationProvider } from './NotificationContext';
+import ToggleSwitch from './components/toggleSwitch/toggleSwitch.js';
 
 const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
   return (
     <NotificationProvider>
-    <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/shopping-detail/:id" element={<ShoppingDetail />} />
-        </Routes>
-    </BrowserRouter>
+      <div className={`App ${isDarkMode ? 'dark-mode' : ''}`}>
+      <ToggleSwitch isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/shopping-detail/:id" element={<ShoppingDetail />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </NotificationProvider>
   );
 };
